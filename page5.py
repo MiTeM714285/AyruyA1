ab3a713bea9d31bc3e10a7e5599228c5fba888da
김예성 <mitem714285@naver.com>
2024-10-21 09:04:25 +0900

    겉모습 5페이지 곡명리스트 추가

--- page5.py
+++ page5.py
@@ -15,17 +15,24 @@ class PageFive(tk.Frame):
         # tk.Frame.configure(self, bg="red")
         fileUrl = "image/" + main.entry['gameAndKeymode'] + ".png"
         img_gameAndKeymode = tk.PhotoImage(file=fileUrl).subsample(2)
-        tk.Label(self, image=img_gameAndKeymode).pack(side="top", pady=30)
+        tk.Label(self, image=img_gameAndKeymode).pack(side="top", pady=(30,15))
         tk.Label.image = img_gameAndKeymode
 
         tk.Label(self, text="곡 랜덤 추첨 결정", font=("Helvetica", 18, "bold")).pack(
             side="top", fill="x", pady=(30, 5)
         )
         tk.Label(self,
-                 text="추첨된 레벨 범위 내로 과제곡을 랜덤으로 추첨합니다.",
+                 text="추첨된 레벨 범위 내로 다음 중 과제곡을 랜덤으로 추첨합니다.",
                  font=("Helvetica", 16, "bold")).pack(
-            side="top", fill="x", pady=20
+            side="top", fill="x", pady=(10,10)
         )
+
+        music1 = tk.Label(self, text="곡명1 [난이도]", font=("Helvetica", 25, "bold"))
+        music2 = tk.Label(self, text="곡명2 [난이도]", font=("Helvetica", 25, "bold"))
+        music3 = tk.Label(self, text="곡명3 [난이도]", font=("Helvetica", 25, "bold"))
+        music1.pack(side="top", fill="x")
+        music2.pack(side="top", fill="x")
+        music3.pack(side="top", fill="x", pady=(0, 20))
 
         tk.Label(self,
                  text="주의:추첨 시작 버튼은 한 번만 유효하며, 다시는 되돌이킬 수 없습니다.",
