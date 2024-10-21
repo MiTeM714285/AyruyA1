2d1effc9dfa31324fbf787bda8c11b912bfa7acf
김예성 <mitem714285@naver.com>
2024-10-21 10:39:44 +0900

    5페이지 추첨 후 결과 고정

--- dbConnect.py
+++ dbConnect.py
@@ -7,3 +7,10 @@ def findLevelResult(gameAndKeymode, level):
     cursor.execute("select result from level_mst where gameAndKeymode = ? and level = ?", (gameAndKeymode, level))
     row = cursor.fetchone()
     return row[0]
+
+def findMusicAndDifficulty(gameAndKeymode, level):
+    conn = sqlite3.connect('DB/ayruya.db3')
+    cursor = conn.cursor()
+    cursor.execute("select musicname, difficulty from tunes_mst where gameAndKeymode = ? and level = ?", (gameAndKeymode, level))
+    row = cursor.fetchall()
+    return row
--- page5.py
+++ page5.py
@@ -5,12 +5,20 @@ import page4
 import page6
 import random
 
+from dbConnect import findMusicAndDifficulty
 from switchFrame import switch_frame
 
 
 class PageFive(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+
+        def randomMusicAndDifficulty():
+            musicButton["state"] = tk.DISABLED
+            musicAndDifficultyIndex = random.randrange(0, 3)
+            main.entry['musicname'] = musicAndDifficulty[musicAndDifficultyIndex][0]
+            main.entry['difficulty'] = musicAndDifficulty[musicAndDifficultyIndex][1]
+            musicResultText.config(text="결과:"+musicAndDifficulty[musicAndDifficultyIndex][0] + " [" + musicAndDifficulty[musicAndDifficultyIndex][1] + "]")
 
         # tk.Frame.configure(self, bg="red")
         fileUrl = "image/" + main.entry['gameAndKeymode'] + ".png"
@@ -43,7 +51,7 @@ class PageFive(tk.Frame):
         musicResultText = tk.Label(self, text="결과:", font=("Helvetica", 30, "bold"))
         musicResultText.pack(side="top", fill="x", pady=20)
 
-        musicButton = tk.Button(self, text="추첨 시작", font=("Helvetica", 24, "bold"))
+        musicButton = tk.Button(self, text="추첨 시작", font=("Helvetica", 24, "bold"), command=lambda: randomMusicAndDifficulty())
         musicButton.pack(pady=(0, 50))
 
         btn_back = tk.Button(self, text="뒤로", font=("Helvetica", 20, "bold"),
@@ -52,3 +60,17 @@ class PageFive(tk.Frame):
                              command=lambda: switch_frame(master, page6.PageSix))
         btn_back.pack(side="left", padx=(280, 0))
         btn_next.pack(side="right", padx=(0, 280))
+
+        # 초기 셋팅
+        musicAndDifficulty = findMusicAndDifficulty(main.entry['gameAndKeymode'], main.entry['level'])
+        music1.config(text=musicAndDifficulty[0][0] + " [" + musicAndDifficulty[0][1] + "]")
+        music2.config(text=musicAndDifficulty[1][0] + " [" + musicAndDifficulty[1][1] + "]")
+        music3.config(text=musicAndDifficulty[2][0] + " [" + musicAndDifficulty[2][1] + "]")
+
+        if main.entry['musicname'] != '':
+            musicButton['state'] = tk.DISABLED
+            musicResultText.config(text="결과:" + main.entry['musicname'] + " [" + main.entry['difficulty'] + "]")
+        else:
+            musicResultText.config(text="결과:")
+
+
